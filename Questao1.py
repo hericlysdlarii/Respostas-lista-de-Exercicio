67568afeb8cf589800b455c2a13bb7c57fb1536c
Hériclys D'Larii <38103867+hericlysdlarii@users.noreply.github.com>
2019-09-03 16:07:32 -0300

    Add files via upload

---
+++ Questao1.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça dois números e imprima o maior deles
+
+a = input("Entre com um numeros!")
+b = input("Entre com outro numeros!")
+if (a > b):
+    print(a)
+if (b > a):
+    print(b)
